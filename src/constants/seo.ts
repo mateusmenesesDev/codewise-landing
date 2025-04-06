3618c607410774108e1a2e79133b8fe84ee880c1
Mateus <mateusppa10@gmail.com>
2025-04-05 21:53:07 -0300

    Feat: add SEO constants and integrate into Layout for improved metadata management

---
+++ src/constants/seo.ts
@@ -0,0 +1,21 @@
+export const pageTitle = 'CodeWise';
+export const pageDescription =
+	'Elevate Your Full Stack Journey with Expert Mentorship';
+export const keywords = [
+	'full stack development',
+	'mentorship',
+	'mentoring',
+	'software mentor',
+	'coding',
+	'programming',
+	'web development',
+	'software development',
+	'software engineering',
+	'software engineer',
+	'software engineer',
+	'software engineer',
+	'software engineer',
+	'software engineer'
+];
+export const author = 'CodeWise';
+export const url = 'https://codewise.online';
